c908535bf760ea3f57563b91a7539a98978bcd13
Tasfin <tareque20@gmail.com>
2018-10-09 20:37:35 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# supertest_node_express_mongodb
+# nodejs express mongodb Passport
+Login app example with MongoDB, Joi validation, Handlebar Template Engine, Passport
+
+# Run Project:
+```sh
+$ npm init
+$ npm install
+```
+```sh
+$ sudo npm install -g nodemon
+```
+If getting error : [nodemon] Internal watch failed: watch ENOSPC
+```sh
+sudo sysctl fs.inotify.max_user_watches=582222 && sudo sysctl -p
+```
+
+```sh
+$ nodemon
+```
+
+# Install MongoDB
+[Install Mongo in Ubuntu](https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+
+
+# MogoDB CMD
+```sh
+$ sudo service mongod start
+$ sudo service mongod stop
+$ sudo service mongod restart
+$ mongo --host 127.0.0.1:27017
+```
+
+# Uninstall MongoDB
+Remove Packages
+```sh
+$ sudo apt-get purge mongodb-org*
+```
+Remove Data Directory
+```sh
+$ sudo rm -r /var/log/mongodb
+$ sudo rm -r /var/lib/mongodb
+
+```
+Log: /var/log/mongodb/mongod.log
+
+# Create Mongo Database
+```sh
+$ sudo service mongod start
+$ mongo --host 127.0.0.1:27017
+```
+```sh
+$ show dbs
+$ use loginapp
+$ db.createCollection('users')
+$ db.books.insert({name : "User 1", email : "user1@gmail.com", username : "user1", password : "123"});
+$ db.books.find().pretty()
+```
+More info: https://docs.mongodb.com/manual/crud/
+
+# Run Mongo Database
+```sh
+$ sudo service mongod start
+```
+
+# API Routes:
+
+```sh
+get: /
+post: /user/login
+post: /user/register
+get: /user/logout
+```
+### Use postman to test api
---
+++ routes/users.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var Joi = require('joi');
+var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+
+var User = require('../models/user');
+
+// Register
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+
+// Login
+router.get('/login', function (req, res) {
+    res.render('login');
+});
+
+// dashboard
+router.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+});
+
+// Register
+router.post('/register', function (req, res) {
+    var name = req.body.name;
+    //var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var confpassword = req.body.confpassword;
+
+    const schema = {
+        name: Joi.string().min(3).required(),
+        //email: Joi.string().min(3),
+        username: Joi.string().min(3).required(),
+        password: Joi.string().min(3).required(),
+        confpassword: Joi.string().min(3).required(),
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error){
+        res.render('register', {
+            errors: result.error.details[0].message
+        });
+    }
+    else{
+        var newUser = new User({
+            name: name,
+            //email: email,
+            username: username,
+            password: password
+        });
+
+        User.createUser(newUser, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+        });
+
+        req.flash('success_msg', 'Registration has been successfully completed');
+        res.redirect('/users/login');
+    }
+
+});
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                return done(null, false, { message: 'Unknown User' });
+            }
+
+            User.comparePassword(password, user.password, function (err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'Invalid password' });
+                }
+            });
+        });
+    }));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.getUserById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/users/dashboard', failureRedirect: '/users/login', failureFlash: true }),
+    function (req, res) {
+    console.log(req);
+        res.redirect('/users/dashboard');
+    });
+
+router.get('/logout', function (req, res) {
+    req.logout();
+
+    req.flash('success_msg', 'You are logged out');
+
+    res.redirect('/users/login');
+});
+
+module.exports = router;
